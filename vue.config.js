127b44a7d8b3c84dfa128b218988cd7ed65a5c48
svarela <sergioalberto.varelab@gmail.com>
2024-09-15 16:07:03 -0600

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/vapes_adminweb/',
   transpileDependencies: true,
   devServer: {
     proxy: 'https://9349-2806-2f0-34e0-a285-c0a9-1b01-19c6-59b3.ngrok-free.app',
