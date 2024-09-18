096da28b8129d0dcaf601243e4c855288515fe2b
svarela <sergioalberto.varelab@gmail.com>
2024-09-17 19:56:51 -0600

    new proxy

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   publicPath: '/VapesAdminWeb/',
   transpileDependencies: true,
   devServer: {
-    proxy: 'https://284d-2806-2f0-34e0-a285-bdae-9098-b4ae-3c33.ngrok-free.app',
+    proxy: 'https://vapesadmin.onrender.com',
 }
 })
