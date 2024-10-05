8a5cf9ab2ea37135d8b913fa238fb0dd06f489b7
svarela <sergioalberto.varelab@gmail.com>
2024-10-05 12:34:58 -0600

    axios router

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import router from '../Router/router';  // Import the router
 
 const axiosInstance = axios.create({
   // dev
@@ -10,5 +11,21 @@ const axiosInstance = axios.create({
     'Access-Control-Allow-Origin': '*'  // Adding CORS header
   },
 });
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    return response;
+  },
+  (error) => {
+    // If the response status is 403, redirect to login page
+    if (error.response && error.response.status === 403) {
+      router.push('/');  // Navigate to login page
+    }
+    // Return any other errors as a rejection
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
